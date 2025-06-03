963dbe792c9a542cbd60b849198a02cd512936ce
EdwinDev99 <edwindev99@gmail.com>
2025-06-03 11:18:22 +0100

    search field

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -1,7 +1,24 @@
+import {
+  Container,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { IoMdSearch } from "react-icons/io";
+
 type Props = {};
 
 function Header({}: Props) {
-  return <div>Header</div>;
+  return (
+    <Container maxW="3xl" mt="1">
+      <InputGroup>
+        <InputLeftElement pointerEvents="none">
+          <IoMdSearch color="gray" />
+        </InputLeftElement>
+        <Input type="text" placeholder="Intenta con 'Chicken o 'beans...''" />
+      </InputGroup>
+    </Container>
+  );
 }
 
 export default Header;
